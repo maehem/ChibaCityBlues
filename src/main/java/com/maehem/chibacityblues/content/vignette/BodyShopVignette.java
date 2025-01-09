82ffe36058ff5c337306f8fdb9fb2afe456a7e4e
Mark J Koch <maehem@users.noreply.github.com>
2025-01-08 21:49:09 -0800

    Remove unused comments.

--- src/main/java/com/maehem/chibacityblues/content/vignette/BodyShopVignette.java
+++ src/main/java/com/maehem/chibacityblues/content/vignette/BodyShopVignette.java
@@ -89,9 +89,6 @@ public class BodyShopVignette extends Vignette {
         // TODO:   Check that file exists.  The current exception message is cryptic.
         LOGGER.config("Add skin for npc. " + NPC_POSE_SHEET_FILENAME);
         npcCharacter.setSkin(getClass().getResourceAsStream(NPC_POSE_SHEET_FILENAME), 1, 4);
-        // Dialog
-        // Load dialog tree from file.
-//        barOwnerCharacter.getDialog().init(getWidth(), getHeight());
 
         initNpcDialog();
 
