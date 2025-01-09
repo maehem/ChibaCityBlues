0333c9fcfd59214962b4745b33a247af33be5e4f
Mark J Koch <maehem@users.noreply.github.com>
2025-01-08 21:52:17 -0800

    Reformat whitespace. Get Street Chatsubo dialog working.

--- src/main/java/com/maehem/chibacityblues/content/vignette/StreetChatsuboVignette.java
+++ src/main/java/com/maehem/chibacityblues/content/vignette/StreetChatsuboVignette.java
@@ -13,7 +13,7 @@
     WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
     License for the specific language governing permissions and limitations
     under the License.
-*/
+ */
 package com.maehem.chibacityblues.content.vignette;
 
 import static com.maehem.abyss.Engine.LOGGER;
@@ -22,18 +22,23 @@ import com.maehem.abyss.engine.Player;
 import com.maehem.abyss.engine.PoseSheet;
 import com.maehem.abyss.engine.Vignette;
 import com.maehem.abyss.engine.VignetteTrigger;
+import com.maehem.abyss.engine.babble.BabbleNode;
+import com.maehem.abyss.engine.babble.DialogBabbleNode;
+import static com.maehem.abyss.engine.babble.DialogCommand.DIALOG_CLOSE;
+import static com.maehem.abyss.engine.babble.DialogCommand.TO_JAIL;
 import com.maehem.abyss.engine.babble.DialogPane;
-import com.maehem.abyss.engine.babble.DialogResponse2;
-import com.maehem.abyss.engine.babble.DialogResponseAction;
-import com.maehem.abyss.engine.babble.DialogSheet2;
 import com.maehem.chibacityblues.content.goal.RatzPaidGoal;
+import java.util.ArrayList;
 import java.util.Properties;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.geometry.Point2D;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 /**
  *
@@ -45,23 +50,20 @@ public class StreetChatsuboVignette extends Vignette {
     private static final String MUSIC = "/content/audio/music/Audiorezout - Cepheus.mp3";
     //private static final String PROP_NAME = "street2";
     private static final String CONTENT_BASE = "/content/vignette/street-chatsubo/";
-    private static final String SKYLINE_IMAGE_FILENAME   = CONTENT_BASE + "skyline.png";
+    private static final String SKYLINE_IMAGE_FILENAME = CONTENT_BASE + "skyline.png";
+    private static final String NPC_CAMEO_FILENAME = CONTENT_BASE + "npc-cameo.png";
     private static final String ROBOT_POSE_SHEET_FILENAME = CONTENT_BASE + "police-robot.png";
     //private static final String DOOR_PATCH_IMAGE_FILENAME = CONTENT_BASE + "door-right-wing.png";
     public static final Point2D PLAYER_START = new Point2D(0.50, 0.86);
     public static final Point2D PLAYER_START_R = new Point2D(0.80, 0.86);
 
     private static final double[] WALK_BOUNDARY = {
-             0.05, 0.9,     0.11, 0.8,
-             0.32, 0.7,    0.6, 0.7,
-             0.65, 0.65,      0.73, 0.65,
-             0.8, 0.70,    0.99, 0.70,
-             0.99, 0.98,     0.05, 0.98
+        0.05, 0.9, 0.11, 0.8,
+        0.32, 0.7, 0.6, 0.7,
+        0.65, 0.65, 0.73, 0.65,
+        0.8, 0.70, 0.99, 0.70,
+        0.99, 0.98, 0.05, 0.98
     };
-
-//    private static final Patch pawnShopDoorPatch = new Patch(
-//            1140, 0, 500,
-//            StreetChatsuboVignette.class.getResourceAsStream(DOOR_PATCH_IMAGE_FILENAME));
 
     private static final VignetteTrigger rightDoor = new VignetteTrigger(
             0.98, 0.66, // Location
@@ -81,9 +83,16 @@ public class StreetChatsuboVignette extends Vignette {
             ChatsuboBarVignette.class
     );
 
+    private static final ArrayList<BabbleNode> DIALOG_CHAIN = new ArrayList<>() {
+        {
+            add(new DialogBabbleNode(DIALOG_CLOSE.num)); // 0   "You're under arrest!"
+            add(new DialogBabbleNode(TO_JAIL.num));      // 1    "Move along..."
+        }
+    };
+
     private final Media media = new Media(getClass().getResource(MUSIC).toExternalForm());
 
-    private com.maehem.abyss.engine.Character policeRobotCharacter;
+    private com.maehem.abyss.engine.Character npcCharacter;
     private int npcAnimationCount = 0;
 
     public StreetChatsuboVignette(GameState gs, VignetteTrigger prevPort, Player player) {
@@ -92,48 +101,54 @@ public class StreetChatsuboVignette extends Vignette {
     }
 
     @Override
-    public void loop() {
-        npcAnimationCount++;
-        if (npcAnimationCount > 40) {
-            npcAnimationCount = 0;
-            policeRobotCharacter.nextPose();
-        }
-    }
-
-    @Override
     protected void init() {
         setHorizon(0.31);
-        // set player position
-        //setPlayerPosition(PLAYER_START);
-
         addPort(rightDoor); // Doors
         addPort(topDoor);
 
-        initBackground();
+        initDialogText(DIALOG_CHAIN);
         initNpc();
 
+        setDialogPane(new DialogPane(this, npcCharacter));
+        initNpcDialog();
+
+        initBackground();
+
+        getDialogPane().setCurrentDialog(dialogWarmUp());
+        getDialogPane().setVisible(false);
+
         topDoor.setLocked(getGameState().hasGoal(RatzPaidGoal.class));
+
+        // Open the dialog after a delay.
+        final Timeline timeline = new Timeline(new KeyFrame(
+                Duration.seconds(2),
+                e -> getDialogPane().setVisible(true)
+        ));
+        timeline.play();
 
         // example: Depth of field
         //fgGroup.setEffect(new BoxBlur(10, 10, 3));
     }
 
     private void initNpc() {
-        policeRobotCharacter = new com.maehem.abyss.engine.Character(bundle.getString("character.robot.name"));
-        policeRobotCharacter.setScale(2.6);
-        policeRobotCharacter.setLayoutX(250);
-        policeRobotCharacter.setLayoutY(520);
-        policeRobotCharacter.setEffect(new DropShadow(70, 0, 110, new Color(0,0,0,0.5)));
+        npcCharacter = new com.maehem.abyss.engine.Character(bundle.getString("character.npc.name"));
+        npcCharacter.setScale(2.6);
+        npcCharacter.setLayoutX(250);
+        npcCharacter.setLayoutY(520);
+        npcCharacter.setEffect(new DropShadow(70, 0, 110, new Color(0, 0, 0, 0.5)));
+
+        LOGGER.config("Apply Cameo for Judge. " + NPC_CAMEO_FILENAME);
+        npcCharacter.setCameo(getClass().getResourceAsStream(NPC_CAMEO_FILENAME));
 
         // TODO:   Check that file exists.  The current exception message is cryptic.
-        policeRobotCharacter.setSkin(PawnShopVignette.class.getResourceAsStream(ROBOT_POSE_SHEET_FILENAME), 1, 4);
-        LOGGER.finer("Add skin for police robot. " + ROBOT_POSE_SHEET_FILENAME);
+        LOGGER.config("Add skin for police robot. " + ROBOT_POSE_SHEET_FILENAME);
+        npcCharacter.setSkin(PawnShopVignette.class.getResourceAsStream(ROBOT_POSE_SHEET_FILENAME), 1, 4);
 
         // Dialog
-        initRobotPoliceDialog();
+        //initNpcDialog();
 
-        getCharacterList().add(policeRobotCharacter);
-        getBgGroup().getChildren().add( policeRobotCharacter );
+        getCharacterList().add(npcCharacter);
+        getBgGroup().getChildren().add(npcCharacter);
     }
 
     private void initBackground() {
@@ -145,32 +160,49 @@ public class StreetChatsuboVignette extends Vignette {
         skylineView.setLayoutX(0);
         skylineView.setLayoutY(0);
         // Add these in visual order.  Back to front.
-        getSkylineGroup().getChildren().add( skylineView );
-
+        getSkylineGroup().getChildren().add(skylineView);
 
     }
 
+    @Override
+    public int dialogWarmUp() {
+        // Check of ChatsuboPaid goal else move along.
+        if (getGameState().hasGoal(RatzPaidGoal.class)) {
+            return 1;
+        }
+        return 0; // Unser arrest.
+    }
+
     // TODO:  Ways to automate this.   JSON file?
-    private void initRobotPoliceDialog() {
-        DialogPane dp = getDialogPane();
-        policeRobotCharacter.setAllowTalk(true);
+    private void initNpcDialog() {
+        //DialogPane dp = getDialogPane();
+        npcCharacter.setAllowTalk(true);
+        getDialogPane().setDialogChain(DIALOG_CHAIN);
         // Eddie kicks the player out of the shop but gives him his item.
-        DialogResponseAction exitAction = () -> {
-            dp.setActionDone(true);
-        };
+//        DialogResponseAction exitAction = () -> {
+//            dp.setActionDone(true);
+//        };
 
-        DialogSheet2 ds1= new DialogSheet2(dp);
-        ds1.setDialogText(bundle.getString("dialog.ds1.robot"));
-        ds1.addResponse(new DialogResponse2(bundle.getString("dialog.ds1.p.1"), exitAction)); // Exit action
+//        DialogSheet2 ds1 = new DialogSheet2(dp);
+//        ds1.setDialogText(bundle.getString("dialog.ds1.robot"));
+//        ds1.addResponse(new DialogResponse2(bundle.getString("dialog.ds1.p.1"), exitAction)); // Exit action
+//
+//        dp.addDialogSheet(ds1);
+    }
 
-        dp.addDialogSheet(ds1);
+    @Override
+    public void loop() {
+        npcAnimationCount++;
+        if (npcAnimationCount > 40) {
+            npcAnimationCount = 0;
+            npcCharacter.nextPose();
+        }
     }
 
 //    @Override
 //    public String getPropName() {
 //        return PROP_NAME;
 //    }
-
     @Override
     public Properties saveProperties() {
         Properties p = new Properties();
